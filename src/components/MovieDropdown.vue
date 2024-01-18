5b127d8031670ffe871530dbe9ccaff9ecaaab80
The.Real.Slim.Tony <antoniomendezrojascr@gmail.com>
2024-01-17 22:44:13 -0600

    loading spinner added

--- src/components/MovieDropdown.vue
+++ src/components/MovieDropdown.vue
@@ -6,7 +6,15 @@
       </option>
     </select>
     <button @click="submit">Enviar</button>
-    <div v-if="response">
+
+    <!-- Indicador de carga -->
+    <div v-if="isLoading" class = "spinner-container">
+      <div class="spinner"></div>
+      <!-- Aquí puedes poner un spinner o alguna otra indicación visual -->
+    </div>
+
+    <!-- Contenido cargado, mostrado solo si no está cargando -->
+    <div v-else-if="response">
       <p>Cartelera:</p>
       <ul class="movie-list">
         <li v-for="(pelicula, index) in response" :key="index" class="movie-item">
@@ -21,6 +29,7 @@
   </div>
 </template>
 
+
 <script>
 import axios from 'axios';
 
@@ -33,18 +42,21 @@ export default {
         { value: 'cartago-costa-rica', text: 'cartago-costa-rica' },
         // ... otras opciones
       ],
+      isLoading:false,
       response: null, // Agrega esta línea para almacenar la respuesta del servidor
     };
   },
   methods: {
     async submit() {
       try {
+        this.isLoading = true;
         const payload = { location: this.selectedOption };
         const res = await axios.post('http://127.0.0.1:8000/cartelera/', payload);
         this.response = res.data; // Almacena la respuesta en la propiedad de datos
-        console.log(this.response);
+        this.isLoading = false;
       } catch (error) {
         console.error(error);
+        this.isLoading = false;
       }
     },
   },
@@ -80,5 +92,39 @@ export default {
 
 .movie-info {
   margin: 5px 0;
+}
+.spinner {
+  border: 4px solid rgba(255, 255, 255, 0.3);
+  border-radius: 50%;
+  border-top: 4px solid #3498db;
+  width: 40px;
+  height: 40px;
+  -webkit-animation: spin 2s linear infinite; /* Safari */
+  animation: spin 2s linear infinite;
+}
+.spinner-container {
+  display: flex;
+  justify-content: center;  /* Centrado horizontal */
+  align-items: center;      /* Centrado vertical */
+  position: fixed;          /* Posición fija */
+  top: 0;
+  left: 0;
+  width: 100%;              /* Ocupar toda la anchura */
+  height: 100%;             /* Ocupar toda la altura */
+  z-index: 1000;            /* Asegurarse de que esté por encima de otros elementos */
+  background-color: rgba(255, 255, 255, 0.8); /* Fondo semitransparente (opcional) */
+}
+
+
+/* Animación de Safari */
+@-webkit-keyframes spin {
+  0% { -webkit-transform: rotate(0deg); }
+  100% { -webkit-transform: rotate(360deg); }
+}
+
+/* Animación para otros navegadores */
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
 }
 </style>
